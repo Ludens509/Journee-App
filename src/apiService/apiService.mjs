9b4f721274993b428458ce483564340d3a566229
Ismael Ludens Alexandre <alexandreludens2@gmail.com>
2025-10-22 03:27:14 -0400

    Edit apiService addinfg new fetch function for CRUD

--- src/apiService/apiService.mjs
+++ src/apiService/apiService.mjs
@@ -10,4 +10,35 @@ async function getUser(token) {
   return res.data;
 }
 
-export default { getUser };
+async function getPostById(id, token) {
+  let res = await axios.get(`${baseURL}/posts/${id}`, {
+    headers: { "x-auth-token": token },
+  });
+  return res.data;
+}
+
+
+async function createPost(postData, token) {
+  const res = await axios.post(`${baseURL}/posts`, postData, {
+    headers: { "x-auth-token": token },
+  });
+  return res.data;
+}
+
+async function updatePost(id, postData, token) {
+  const res = await axios.put(`${baseURL}/posts/${id}/edit`, postData, {
+    headers: { "x-auth-token": token },
+  });
+
+  return res.data;
+}
+
+async function deletePost(id, token) {
+  const res = await axios.delete(`${baseURL}/posts/${id}`, {
+    headers: { "x-auth-token": token },
+  });
+
+  return res.data;
+}
+
+export default { getUser, getPostById, createPost, updatePost, deletePost };
