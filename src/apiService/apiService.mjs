6bd37e399a31dc6d0928821ef4c5d9a499fc01cb
Ismael Ludens Alexandre <alexandreludens2@gmail.com>
2026-03-17 02:35:41 -0400

    Change baseURL for production

--- src/apiService/apiService.mjs
+++ src/apiService/apiService.mjs
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-// let baseURL = `https://journee-backend.onrender.com/api`;
-let baseURL = `http://localhost:3000/api`; // Change to local for development
+let baseURL = `https://journee-backend.onrender.com/api`;
 
 async function getUser(token) {
   let res = await axios.get(`${baseURL}/auth`, {
